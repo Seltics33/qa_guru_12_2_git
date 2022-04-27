e4d9c367848c281fdb865be5fc6569464273aaec
Seltics33 <103753798+Seltics33@users.noreply.github.com>
2022-04-28 00:12:34 +0500

    Update Main.java

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -6,4 +6,8 @@ public class Main {
         System.out.println("hello, QA.GURU!");
         System.out.println("Another line!");
     }
+    
+    public static void another() {
+        System.out.println("Hello, QA.Guru")
+    }
 }

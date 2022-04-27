a0ac8dbfa1cf6cafd3346e14cbbf26c228b4fa8a
Rahimov Albert <rahimov.al@mail.ru>
2022-04-27 22:48:24 +0500

    switch to apper case

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -3,6 +3,6 @@ package guru.qa;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello, QA.guru!");
+        System.out.println("hello, QA.GURU!");
     }
 }
